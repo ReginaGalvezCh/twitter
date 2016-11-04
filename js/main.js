18df5c1d81a8eadcbea034f67e3e8f0e7fd1a09a
Regina Galvez Chavez <reginagalvezchavez@MBP-de-Regina.iptv.microsoft.com>
2016-11-04 19:50:15 -0300

    indentado

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
 
-//crear boton e items
-var milista = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < milista.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\uD83D\uDDD1");
-  span.className = "cerrar";
-  span.appendChild(txt);
-  milista[i].appendChild(span);
-}
 
 // Click en el btn para cerrar lista
 var cerrar = document.getElementsByClassName("cerrar");
 var i;
 for (i = 0; i < cerrar.length; i++) {
-    cerrar[i].onclick = function() {
+  cerrar[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
-}
+  }
 }
 
 // agregar visto cuando se selecciona un elemto
@@ -25,7 +15,7 @@ var lista = document.querySelector('ul');
 lista.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
-}
+  }
 }, false);
 
 // crear lista nueva cuando le doy al boton 
@@ -37,21 +27,22 @@ function nuevaTarea() {
   li.appendChild(t);
   if (ingresoValor === '') {
     alert("Hola escribe algo!");
-} else {
+  } else {
     document.getElementById("myUL").appendChild(li);
-}
-document.getElementById("miInput").value = "";
+  }
+  document.getElementById("miInput").value = "";
 
-var span = document.createElement("SPAN");
-var txt = document.createTextNode("\uD83D\uDDD1");
-span.className = "cerrar";
-span.appendChild(txt);
-li.appendChild(span);
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\uD83D\uDDD1");
+  span.className = "cerrar";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-for (i = 0; i < cerrar.length; i++) {
+  for (i = 0; i < cerrar.length; i++) {
     cerrar[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
+    }
   }
 }
-}
+
